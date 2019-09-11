a388e7f57bb5f95df4e7ef92ac8ad7cf9ff52773
jakabij <55176543+jakabij@users.noreply.github.com>
2019-09-11 08:46:59 +0200

    Create var3.py

---
+++ var3.py
@@ -0,0 +1,8 @@
+num1=int(input("Add the first number! "))
+num2=int(input("Add the second number! "))
+print("")
+suma=num1//num2
+left=num1%num2
+
+print("The integer part:",suma)
+print("The left over part:" ,left)
