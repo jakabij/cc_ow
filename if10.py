7e27be10bd1d67f4e7298427b8c4faa9ad6748f6
jakabij <55176543+jakabij@users.noreply.github.com>
2019-09-11 09:30:24 +0200

    Create if10.py

---
+++ if10.py
@@ -0,0 +1,21 @@
+a=input("Give the a side! ")
+b=input("Give the b side! ")
+c=input("Give the c side! ")
+
+if a.isdigit() and b.isdigit() and c.isdigit():
+	a=int(a)
+	b=int(b)
+	c=int(c)
+	if not(a==0) and not(b==0) and not(c==0):
+		if (a**2)+(b**2)==(c**2):
+			print("Editable!")
+		elif (a**2)+(c**2)==(b**2):
+			print("Editable!")
+		elif (c**2)+(b**2)==(a**2):
+			print("Editable!")
+		else:
+			print("Not editable!")
+	else:
+		print("Give non zero sides!")
+else:
+	print("Only numbers allowed!")
