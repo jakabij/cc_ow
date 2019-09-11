901281578ca4d5d13dd0d8694b2a57d5e27507c7
jakabij <55176543+jakabij@users.noreply.github.com>
2019-09-11 09:27:29 +0200

    Create if9.py

---
+++ if9.py
@@ -0,0 +1,15 @@
+a=input("Give the a side! ")
+b=input("Give the b side! ")
+c=input("Give the c side! ")
+
+if a.isdigit() and b.isdigit() and c.isdigit():
+	a=int(a)
+	b=int(b)
+	c=int(c)
+	if not(a==0) and not(b==0) and not(c==0):
+		if (a**2)+(b**2)==(c**2):
+			print("Editable!")
+		else:
+			print("Not editable!")
+else:
+	print("Only numbers allowed!")
